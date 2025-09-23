c5a04afa484bb893c38788a4885795c461483564
Vince <vince@MacBook-Air-de-Vince.local>
2025-09-24 00:58:18 +0200

    tata

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000"
+const API_URL = "https://nounou-app-production.up.railway.app"
 
 async function ajouterEntree() {
     let date = document.getElementById("date").value;
